d732a2e8525a423fd2a977134ea2db216e723f98
Vibhu Dixit <vibhudixit.2215001940@gmail.com>
2025-02-08 07:20:23 +0530

    ए पहुना एहि मिथिले में रहु ना राम सीता विवाह

---
+++ Bitmasking/Type3/Minimum Number of Operations to Make Array XOR Equal to K.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(int[] arr, int k) {
+        int ans = Arrays.stream(arr).reduce(0, (a, b) -> a ^ b);
+        return Integer.bitCount(ans^k);
+    }
+}
+class Solution {
+    public int minOperations(int[] arr, int k) {
+        int ans = 0;
+        for(int x:arr) ans ^=x;
+        return Integer.bitCount(ans^k);
+    }
+}
+
+ChatGPT
